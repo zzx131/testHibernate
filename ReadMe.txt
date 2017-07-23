a7599f36d4677b0b209eccaa7df2f7416b5b47b8
zhangzexin <zhangzexin@deepdata.com.cn>
2017-07-23 11:11:36 +0800

    添加获取当前线程下的session，测试query类和sqlQuery类的查询

---
+++ ReadMe.txt
@@ -0,0 +1,49 @@
+1,实体状态有三种
+1）瞬时态
+对象里面没有id，没有与session进行关联
+2）持久态
+对象里面有id值，对象与session有关联
+3）托管态
+对象里面有id值，没有与session关联
+2，Hibernate的一级缓存
+什么是缓存
+1)，数据存放到数据库里面，数据库本身是文件系统，使用流方式操作文件效率不是很高
+2),吧数据放到内存中，提供读取效率
+
+Hibernate缓存
+1，hibernate的一级缓存
+   (1）hibernate的一级缓存默认是打开的
+   (2) hibernate的使用的范围是，是session的范围从session创建到session关闭范围
+   (3) hibernate的一级缓存中，存储数据是必须的持久数据。
+
+   一级缓存的特性
+   1，持久态自动更新，不需要update
+2，hibernate的二级缓存
+    （1）目前不在使用，替代技术是redis
+    （2）二级缓存默认不是打开的，需要配置
+    （3）二级缓存的使用范围是，sessionFaction的范围
+3，验证一级缓存存在
+    （1）首先根据uid=1查询，返回数据
+    （2）第二查询的时候从缓存中进行查询
+
+Hibernate绑定session
+1，帮我们实现本地线程绑定session
+2，获取与本地线程session
+1）在hibernate核心配置文件中添加配置
+2）用hibernate中有一个方法进行调用
+
+
+
+Hibernate的api使用
+
+1，Query对象
+    不需要写sql语句但是的写hql语句
+    查询所有的hql语句
+    （1）from 实体对象名称
+（1）创建Query对象
+（2）写hql语句
+2，Criteria对象
+    1),使用这个对象查询操作，但是使用这个对象的时候，不需要写语句，直接调用方法就可以了
+    2),创建对象Criteria对象
+3，SQLQuery对象
+    1,使用hibernate时候，调用底层sql实现。
--- test/testDemo.java
+++ test/testDemo.java
@@ -1,14 +1,15 @@
 import cn.com.baomidou.testHibernate.entity.UserEntity;
 import cn.com.baomidou.testHibernate.util.HibernateUtils;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
+import org.hibernate.*;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.io.File;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by zzx on 2017/7/21.
@@ -18,11 +19,98 @@ public class testDemo {
     private Session session;
     private Transaction tx;
 
-    @Before
+    /*@Before
     public void start() {
         sessionFactory = HibernateUtils.getSessionFaction();
         session = sessionFactory.openSession();
         tx = session.beginTransaction();
+    }*/
+
+    @Test
+    public void testTransaction() {
+        try {
+            session = HibernateUtils.getSesionObject();
+            tx = session.beginTransaction();
+
+            UserEntity userEntity = session.get(UserEntity.class, "3");
+            System.out.println(userEntity);
+        } catch (HibernateException e) {
+            tx.rollback();
+            e.printStackTrace();
+        } finally {
+            tx.commit();
+            /*session.close();*/
+        }
+    }
+
+    /**
+     * query对象的测试
+     */
+    @Test
+    public void testQuery() {
+        try {
+            session = HibernateUtils.getSesionObject();
+            tx = session.beginTransaction();
+
+            Query query = session.createQuery("from UserEntity");
+            List<UserEntity> userEntities = query.list();
+
+            for (UserEntity userEntity : userEntities) {
+                System.out.println(userEntity);
+            }
+        } catch (HibernateException e) {
+            tx.rollback();
+            e.printStackTrace();
+        } finally {
+            tx.commit();
+        }
+    }
+
+    /**
+     * 测试Criteria对象
+     */
+    @Test
+    public void testCriteria() {
+        try {
+            session = HibernateUtils.getSesionObject();
+            tx = session.beginTransaction();
+
+            Criteria criteria = session.createCriteria(UserEntity.class);
+            List<UserEntity> userEntities = criteria.list();
+
+            for (UserEntity userEntity : userEntities) {
+                System.out.println(userEntity);
+            }
+        } catch (HibernateException e) {
+            tx.rollback();
+            e.printStackTrace();
+        } finally {
+            tx.commit();
+        }
+    }
+
+    @Test
+    public void testSqlQuery() {
+        try {
+            session = HibernateUtils.getSesionObject();
+            tx = session.beginTransaction();
+
+            SQLQuery sqlQuery = session.createSQLQuery("select * from tb_user");
+            sqlQuery.addEntity(UserEntity.class);
+            List<UserEntity> userEntities=sqlQuery.list();
+            for (UserEntity userEntity:userEntities){
+                System.out.println(userEntity);
+            }
+            /*List<Object[]> userEntities = sqlQuery.list();
+            for (Object[] objects : userEntities) {
+                System.out.println(Arrays.toString(objects));
+            }*/
+        } catch (HibernateException e) {
+            tx.rollback();
+            e.printStackTrace();
+        } finally {
+            tx.commit();
+        }
     }
 
     @Test
@@ -66,14 +154,34 @@ public class testDemo {
         session.saveOrUpdate(userEntity);
     }
 
+    @Test
+    public void testCasch() {
+        UserEntity userEntity = session.get(UserEntity.class, "3");
+        System.out.println(userEntity);
 
-    @After
+        UserEntity userEntity1 = session.get(UserEntity.class, "3");
+        System.out.println(userEntity1);
+    }
+
+    /**
+     * 测试hibernate的缓存特性
+     * 持久态的状态，更新的时候，不需要update
+     */
+    @Test
+    public void testCaschTx() {
+        UserEntity userEntity = session.get(UserEntity.class, "3");
+        userEntity.setUsername("韩梅梅");
+        System.out.println();
+    }
+
+
+    /*@After
     public void finsh() {
         tx.commit();
         session.close();
         sessionFactory.close();
     }
-
+*/
     /*public static void main(String[] args) {
         File file = new File("src/main/resources/Hibernate/hibernate.cfg.xml");
         Configuration cfg = new Configuration();
